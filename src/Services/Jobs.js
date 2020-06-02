a17069fdf43816322c083efb338cdec28617f1b1
Edebo <akandeadebowale0@gmail.com>
2020-06-02 17:40:34 +0100

    adjust axios auth

--- src/Services/Jobs.js
+++ src/Services/Jobs.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { api } from "../constant";
 import { isAuth } from "./Auth";
 
-axios.defaults.headers.Authorization = `Bearer ${isAuth().token}`;
+axios.defaults.headers.common["Authorization"] = `Bearer ${isAuth().token}`;
 
 export const allJobs = async () => {
+  console.log(isAuth().token);
   let res = await axios.get(`${api}/job/all`);
   return res;
 };
